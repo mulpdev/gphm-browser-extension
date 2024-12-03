d4e22e2dec595749b2e2a47693eb712354574983
mulpdev <mulpdev@gmail.com>
2024-12-02 18:11:41 -0600

    scouting profile done

--- mozilla/smartcopy/background.js
+++ mozilla/smartcopy/background.js
@@ -1,16 +1,46 @@
 browser.contextMenus.create({
-        id: "gphm-smart-copy",
-        title: "GPHM Smart Copy",
+        id: "gphm-smart-copy-scouting-profile",
+        title: "GPHM Smart Copy Scouting Profile",
+        contexts: ["page"],
+    },
+    () => void browser.runtime.lastError,
+);
+browser.contextMenus.create({
+        id: "gphm-smart-copy-player-popup",
+        title: "GPHM Smart Copy Player popup",
         contexts: ["selection"],
     },
     () => void browser.runtime.lastError,
 );
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "gphm-smart-copy") {
-
+    if (info.menuItemId === "gphm-smart-copy-player-popup") {
         // gphm-smart-copy.js defines function copyToClipboard.
         const code = "copyToClipboard()";
+        browser.tabs.executeScript({
+            code: "typeof copyToClipboard === 'function';",
+        }).then((results) => {
+            // The content script's last expression will be true if the function
+            // has been defined. If this is not the case, then we need to run
+            // gphm-smart-copy.js to define function copyToClipboard.
+            if (!results || results[0] !== true) {
+                return browser.tabs.executeScript(tab.id, {
+                    file: "gphm-smart-copy.js",
+                });
+            }
+        }).then(() => {
+            return browser.tabs.executeScript(tab.id, {
+                code,
+            });
+        }).catch((error) => {
+            // This could happen if the extension is not allowed to run code in
+            // the page, for example if the tab is a privileged page.
+            console.error("Failed to copy text: " + error);
+        });
+    }
+    else if (info.menuItemId === "gphm-smart-copy-scouting-profile") {
+        // gphm-smart-copy.js defines function copyToClipboard.
+        const code = "copyToClipboard2()";
         browser.tabs.executeScript({
             code: "typeof copyToClipboard === 'function';",
         }).then((results) => {
--- mozilla/smartcopy/gphm-smart-copy.js
+++ mozilla/smartcopy/gphm-smart-copy.js
@@ -81,3 +81,111 @@ function parseValuesOnly(copied) {
     return ret.trim();
 }
 
+function copyToClipboard2(text, html) {
+    function oncopy(event) {
+        document.removeEventListener("copy", oncopy, true);
+        // Hide the event from the page to prevent tampering.
+        event.stopImmediatePropagation();
+
+        modified = ''
+
+        // get scouting data
+        srdiv = document.getElementById('scoutingReports');
+        //console.log(srdiv);
+        mc_all = srdiv.querySelectorAll('.modal-content');
+        //console.log(mc_all);
+        mc = mc_all[0]
+        h3 = mc.querySelector('h3');
+        modified += h3.textContent.substring(19) + ', ';
+        
+        mc = mc_all[1]
+        //console.log(mc);
+        src = mc.querySelector('.scouting-report-container')
+        //console.log('src: ' + src.textContent);
+        notesdiv = src.querySelector('.scouting-report-container__notes');
+        //console.log('notesdiv: ' + notesdiv.textContent);
+        
+        row = mc.querySelector('.row');
+        //console.log(row);
+        col_all = row.querySelectorAll('.column')
+        //console.log(col_all);
+        left = col_all[0];
+        //console.log(left);
+        dli_all = left.querySelectorAll('.data-list__item')
+        //console.log(dli_all);
+        for (i = 0; i < dli_all.length; i++)
+        {
+            dli = dli_all[i]
+            //console.log(dli);
+            // 0-3
+            if (i < 4) {
+                
+                em = dli.querySelector('em')
+                //console.log(em);
+                sibling = em.nextSibling;
+                modified += sibling.textContent.trim() + ', ';
+            }
+            else {
+                // 4-7
+                tmpstr = ''
+                em = dli.querySelector('em')
+                //console.log(em);
+                sibling = em.nextSibling;
+                tmpstr += sibling.textContent.trim() + '|';
+                progress = dli.querySelector('.progress')
+                
+                color = pullGPHMColorFromClassName(progress);
+                if (color !== null) {
+                    tmpstr += color + '|';
+                }
+                span = progress.querySelector('span')
+                tmpstr += span.style.width.trim() + ', ';
+                modified += tmpstr;
+            }
+        }
+        
+        right = col_all[1];
+        advice_lists_all = right.querySelectorAll('.advice-list');
+        console.log(advice_lists_all);
+        
+        for (i = 0; i < advice_lists_all.length; i++)
+        {
+            liked = advice_lists_all[0];
+            likedstr = ''
+            li_all = liked.querySelectorAll('li')
+            for (j = 0; j < li_all.length; j++)
+            {
+                li = li_all[j];
+                name = li.textContent;
+                name = name.substring(0, name.length-3)
+                modified += name + ", "
+                
+                //a = li.querySelector('a')
+                //rel = a.rel;
+                //modified += "&" + rel + ", "
+            }
+        }
+        
+        modified = modified.replaceAll(",", "\t");
+        console.log(modified);
+        
+        // Overwrite the clipboard content.
+        event.preventDefault();
+        event.clipboardData.setData("text/plain",  modified);
+    }
+    document.addEventListener("copy", oncopy, true);
+
+    // Requires the clipboardWrite permission, or a user gesture:
+    document.execCommand("copy");
+}
+
+function pullGPHMColorFromClassName(ele) {
+    findme = ['success', 'ok', 'alert', 'warning'];
+    ret = 'ok';
+    cn_arr = ele.className.split(' ');
+    for (cn of cn_arr) {
+        if (findme.includes(cn)) { ret = cn; break; }
+    }
+    return ret;
+}
+
